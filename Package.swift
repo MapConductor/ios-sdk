7c2c5a22a0504cc3824d542ba0a10df14f892aea
Masashi Katsumata <wf9a5m75@gmail.com>
2026-03-25 00:57:14 +0900

    update Package.swift

---
+++ Package.swift
@@ -0,0 +1,10 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "mapconductor-ios-sdk",
+    platforms: [.iOS(.v17)],
+    products: [],
+    dependencies: [],
+    targets: []
+)
